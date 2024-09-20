396cb4a5fc6b3cc5ca0133ba581ef5c6f2bca896
JeongSeok <ljs0429777@gmail.com>
2024-09-20 21:30:32 +0900

    first commit

---
+++ src/main/java/com/jeongseok/np1practice/Np1PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jeongseok.np1practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Np1PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Np1PracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeongseok/np1practice/Np1PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeongseok.np1practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Np1PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
