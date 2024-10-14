368f3c4d90d7220bb0ea6d3da4f3ab66d461592f
JeongSeok <ljs0429777@gmail.com>
2024-10-14 21:09:59 +0900

    feat: 기본 뼈대 코드 작성

--- src/main/java/com/jeongseok/np1practice/entity/Comment.java
+++ src/main/java/com/jeongseok/np1practice/entity/Comment.java
@@ -1,9 +1,14 @@
 package com.jeongseok.np1practice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,17 +16,28 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "content")
 public class Comment {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	@Column(name = "comment_id")
+	private long commentId;
 
+	@Column(name = "content")
 	private String content;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id")
+	private Post post;
+
 	@Builder
-	public Comment(long id, String content) {
-		this.id = id;
+	public Comment(long commentId, String content) {
+		this.commentId = commentId;
 		this.content = content;
+	}
+
+	public void addPost() {
+		this.post = post;
 	}
 }
--- src/main/java/com/jeongseok/np1practice/entity/Post.java
+++ src/main/java/com/jeongseok/np1practice/entity/Post.java
@@ -1,9 +1,14 @@
 package com.jeongseok.np1practice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,17 +16,34 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "post")
 public class Post {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	@Column(name = "post_id")
+	private long postId;
 
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "content")
 	private String content;
 
+	@OneToMany(mappedBy = "post")
+	private List<Comment> comments = new ArrayList<>();
+
 	@Builder
-	public Post(long id, String content) {
-		this.id = id;
+	public Post(long postId, String title, String content) {
+		this.postId = postId;
+		this.title = title;
 		this.content = content;
+	}
+
+	public void addComment(Comment comment) {
+		if (!comments.contains(comment)) {
+			comments.add(comment);
+			comment.addPost();
+		}
 	}
 }
